2c4c90de3a83ad81014c96faf95ea3413fe666a7
Alessio Seeberger <aseeberger@me.com>
2025-10-13 17:55:18 +0200

    format css

--- src/web/server.js
+++ src/web/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { createProxyMiddleware } from "http-proxy-middleware";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
